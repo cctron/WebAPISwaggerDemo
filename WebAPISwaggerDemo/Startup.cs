5324096c7cc1e64c665055aa8e07e8df86293086
cctron <ajcivicogonzalez@gmail.com>
2018-09-27 00:33:53 +0200

    ficheros del proyecto agregados

---
+++ WebAPISwaggerDemo/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+
+namespace WebAPISwaggerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+            services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc("v1", new Info { Title = "Concert API", Version = "v1" });
+                });
+
+            services.AddDbContext<TicketContext>(opt => opt.UseInMemoryDatabase("TicketList"));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+
+            app.UsePathBase(Configuration["VirtualDirectory"]);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint(Configuration["VirtualDirectory"] + "/swagger/v1/swagger.json", "Ticket API V1");
+            });
+
+            app.UseMvc();
+
+        }
+    }
+}
